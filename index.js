6691043f60a9bb7546070ca9993f2fa495cfbf4d
Isaac <isaacrdmc@gmail.com>
2024-10-05 23:52:40 -0600

    Subo la imágen de los requerimientos al igual que el progreso de las vacaciónes

--- index.js
+++ index.js
@@ -1,183 +1,5 @@
 // Creamos la función calcular el semestre:
-function calcularSemestre() {
-  // Creamos los espacios para las fechas
-  const startDateInput = document.getElementById("startDate").value; // Extraemos los valores del input
-  const endDateInput = document.getElementById("endDate").value;
-  // const horasLunes = Number(document.getElementById("horasLunes").value);
-  // const horasMartes = Number(document.getElementById("horasMartes").value);
-  // const horasMiercoles = Number(
-  //   document.getElementById("horasMiercoles").value
-  // );
-  // const horasJueves = Number(document.getElementById("horasJueves").value);
-  // const horasViernes = Number(document.getElementById("horasViernes").value);
-
-
-
-  // * Declaramos las variables para los datos ingresados
-  const horasLunes = 1;
-  const horasMartes = 2;
-  const horasMiercoles = 4;
-  const horasJueves = 3;
-  const horasViernes = 3;
-
-  let fecha1 = new Date(startDateInput + "T00:00"); // Aseguramos que sera la hora local y tomara las de UTC
-  const fecha2 = new Date(endDateInput + "T00:00");
-  // * Declaramos las variables para las vacaciónes intermedias:
-  let vacaciones1 = new Date(startDateInput + "T00:00");
-  let vacaciones2 = new Date(startDateInput + "T00:00");
-
-
-  const vacaciones = [
-    {start: vacaciones1, end:vacaciones2},
-    {start: vacaciones1, end:vacaciones2},
-  ];
-
-
-  // ? Verificamos si la fecha se encuntra dentro del rango de vaciónes:
-  const sonVacaciónes = (fecha) => {
-    return vacaciones.some(rango => {
-      return fecha >= rango.start && fecha <= rango.end;
-    })
-  }
-
-  
-
-  // ? Sección para los arreglos
-  
-  // * Arreglo que tendra todos los timesatmps de todos los dias de la fecha 1 a la 2
-  const diasTimeStamp = [];
-  // Arrglo que contendra todos los dias del semestre
-  const diaSemestre = [];
-  const tbl = [];
-  const alumnos = 24;
-  // const inHabilesInput = document.getElementById("inhabilDates").value;
-
-  const agregarDiasAlSemestre = function (dia, horas) {
-    for (let i = 0; i < horas; i++) {
-      diaSemestre.push(dia);
-    }
-  };
-
-
-  // ? Sección de las funciónes
-  // 
-  const setearTabla = function (alumnos) {
-    tbl.push([["Alumnos", ...diaSemestre]]);
-    let row = [];
-    for (let i = 0; i <= alumnos; i++) {
-      row.push(`ALumno ${i}`);
-      for (let j = 0; j < diaSemestre.length; j++) {
-        row.push("");
-      }
-      tbl.push(row);
-      row = [];
-    }
-  };
-  // const semestre = new Date(fecha2 - fecha1);
-  // console.log(semestre);
-
-
-
-
-  // ? Sección del código con la lógica
-  if (fecha1 && fecha2) {
-    //! Nos aseguramos que no son el mismo día:
-    if (fecha2 < fecha1) {
-      // document.getElementById("semestreResult").innerText =
-      //   "La fecha de cierre debe de ser posteriror"; // Pendiente, agregar una sweetAlert
-      alert("fechas incorrectas");
-    }
-
-    while (fecha1 < fecha2) {
-      console.log(fecha1);
-
-      // Establecemos los milisegundos de un dia completo para luego agregarlo al timestamp
-      let unDiaEnMilisegundos = 24 * 60 * 60 * 1000;
-      //Agregamos el timestamp de la primera fecha
-      diasTimeStamp.push(fecha1.getTime());
-      // Seteamos el timestamp de la fecha1 un dia mas
-      fecha1.setTime(fecha1.getTime() + unDiaEnMilisegundos);
-    }
-
-    diasTimeStamp.forEach((ts) => {
-      if (!sonVacaciónes(fecha1)) {
-        const dia = new Date(ts).getDay();
-        const numDia = new Date(ts).getDate();
-  
-        if (dia !== 0 && dia !== 6) {
-          switch (dia) {
-            case 1:
-              agregarDiasAlSemestre(numDia, horasLunes);
-              break;
-            case 2:
-              agregarDiasAlSemestre(numDia, horasMartes);
-              break;
-            case 3:
-              agregarDiasAlSemestre(numDia, horasMiercoles);
-              break;
-            case 4:
-              agregarDiasAlSemestre(numDia, horasJueves);
-              break;
-            case 5:
-              agregarDiasAlSemestre(numDia, horasViernes);
-              break;
-          }
-        }
-      }
-    });
-
-
-    // ? Mostramos los datos de los areglos para generar la tabla
-    console.log(diaSemestre);
-
-    setearTabla(alumnos);
-
-    console.log(tbl);
-
-
-
-
-
-
-
-
-    // // ? Almacenamos los datos inhabiles:
-    // const inhabilDias = inHabilesInput
-    //   .split(",")
-    //   .map((date) => new Date(date.trim()));
-    // // ? indicamos cuantos dias habiles tenemos
-    // let diasHabiles = 0;
-    // // ? Le pasamos la variable de inicio para recorrer cada día hasta la fecha de final
-    // let actual = new Date(inicio);
-
-    // // ? Contamos cada día desde la fecha de inicio hasta la fecha final
-    // // ? Esto para excluir los fines de semana y posterirormente los días sin clase
-    // while (actual <= final) {
-    //   //
-    //   const diaSemana = actual.getDay();
-
-    //   // Verificamos si el día es habil o no:
-    //   const noEsHabil = inhabilDias.some(
-    //     (inhabilDias) => actual.toDateString() === inhabilDias.toDateString()
-    //   );
-
-    //   // Solo contamos si no es fin de semana
-    //   if (diaSemana !== 0 && diaSemana !== 6) {
-    //     diasHabiles++;
-    //   }
-    //   // Avanzamos al sigueente día:
-    //   actual.setDate(actual.getDate() + 1);
-    // }
-
-    // ? Mostramos el peridodo:
-    // document.getElementById(
-    //   "semestreResult"
-    // ).innerText = `La cantidad de dáis es: ${diasHabiles}`;
-  } else {
-    // document.getElementById("semestreResult").innerText = `Ingresa las fechas`;
-    alert("ingrese las fechas");
-  }
-}
+// function 
 
 new Vue({
     el: '#app',
@@ -186,25 +8,212 @@ new Vue({
         return {
             showForm: true,
             horasPorDia: '',
+            
             periodoInicio: '',
             periodoFin: '',
+
             periodoEvaluaciones: '',
+            
             vacaciones: '',
             inicioVacaciones: '',
             finVacaciones: '',
             diasAsueto: '',
+            
             cantidadAlumnos: '',
+            
             nombreGrupo: '',
             nombreMateria: '',
             nombreDocente: '',
             alumnos: [],
+            
             vacacionesOptions: ['Sí', 'No'],
             horasPorDiaOptions: [0, 1, 2, 3, 4, 5],
         };
     },
     methods: {
       // ? Agregamso la función principal que se llama en el HTML
-      generarAsistencia(){},
+      calcularSemestre() {
+        // Creamos los espacios para las fechas
+        const startDateInput = this.periodoInicio; // Extraemos los valores del input
+        const endDateInput = this.periodoFin;
+
+        const startVacationInput = this.inicioVacaciones;
+        const endVacationInput = this.finVacaciones;
+        const diasAsuelto = this.diasAsuelto;
+
+        const cantAlumnos = this.cantAlumnos;
+
+        // const horasLunes = Number(document.getElementById("horasLunes").value);
+        // const horasMartes = Number(document.getElementById("horasMartes").value);
+        // const horasMiercoles = Number(document.getElementById("horasMiercoles").value);
+        // const horasJueves = Number(document.getElementById("horasJueves").value);
+        // const horasViernes = Number(document.getElementById("horasViernes").value);     
+        // * Declaramos las variables para los datos ingresados
+        const horasLunes = 1;
+        const horasMartes = 2;
+        const horasMiercoles = 4;
+        const horasJueves = 3;
+        const horasViernes = 3;
+      
+        let fecha1 = new Date(startDateInput + "T00:00"); // Aseguramos que sera la hora local y tomara las de UTC
+        const fecha2 = new Date(endDateInput + "T00:00");
+        // * Declaramos las variables para las vacaciónes intermedias:
+        let vacaciones1 = new Date(startVacationInput + "T00:00");
+        let vacaciones2 = new Date(endVacationInput + "T00:00");
+      
+      
+        // Creamos el arreglo par apoder almacenar los datos de las vacaciónes
+        const vacaciones = [
+          {start: vacaciones1, end:vacaciones2},
+          {start: vacaciones1, end:vacaciones2},
+        ];
+      
+      
+        // ? Verificamos si la fecha se encuntra dentro del rango de vaciónes:
+        const sonVacaciónes = (fecha) => {
+          return vacaciones.some(rango => {
+            return fecha >= rango.start && fecha <= rango.end;
+          })
+        }
+      
+        
+      
+        // ? Sección para los arreglos
+        
+        // * Arreglo que tendra todos los timesatmps de todos los dias de la fecha 1 a la 2
+        const diasTimeStamp = [];
+        // Arrglo que contendra todos los dias del semestre
+        const diaSemestre = [];
+        const tbl = [];
+        const alumnos = 24;
+        // const inHabilesInput = document.getElementById("inhabilDates").value;
+      
+        const agregarDiasAlSemestre = function (dia, horas) {
+          for (let i = 0; i < horas; i++) {
+            diaSemestre.push(dia);
+          }
+        };
+      
+      
+        // ? Sección de las funciónes
+        // 
+        const setearTabla = function (alumnos) {
+          tbl.push([["Alumnos", ...diaSemestre]]);
+          let row = [];
+          for (let i = 0; i <= alumnos; i++) {
+            row.push(`ALumno ${i}`);
+            for (let j = 0; j < diaSemestre.length; j++) {
+              row.push("");
+            }
+            tbl.push(row);
+            row = [];
+          }
+        };
+        // const semestre = new Date(fecha2 - fecha1);
+        // console.log(semestre);
+      
+      
+      
+      
+        // ? Sección del código con la lógica
+        if (fecha1 && fecha2) {
+          //! Nos aseguramos que no son el mismo día:
+          if (fecha2 < fecha1) {
+            // document.getElementById("semestreResult").innerText =
+            //   "La fecha de cierre debe de ser posteriror"; // Pendiente, agregar una sweetAlert
+            alert("fechas incorrectas");
+          }
+      
+          while (fecha1 < fecha2) {
+            console.log(fecha1);
+      
+            // Establecemos los milisegundos de un dia completo para luego agregarlo al timestamp
+            let unDiaEnMilisegundos = 24 * 60 * 60 * 1000;
+            //Agregamos el timestamp de la primera fecha
+            diasTimeStamp.push(fecha1.getTime());
+            // Seteamos el timestamp de la fecha1 un dia mas
+            fecha1.setTime(fecha1.getTime() + unDiaEnMilisegundos);
+          }
+      
+          diasTimeStamp.forEach((ts) => {
+            if (!sonVacaciónes(fecha1)) {
+              const dia = new Date(ts).getDay();
+              const numDia = new Date(ts).getDate();
+        
+              if (dia !== 0 && dia !== 6) {
+                switch (dia) {
+                  case 1:
+                    agregarDiasAlSemestre(numDia, horasLunes);
+                    break;
+                  case 2:
+                    agregarDiasAlSemestre(numDia, horasMartes);
+                    break;
+                  case 3:
+                    agregarDiasAlSemestre(numDia, horasMiercoles);
+                    break;
+                  case 4:
+                    agregarDiasAlSemestre(numDia, horasJueves);
+                    break;
+                  case 5:
+                    agregarDiasAlSemestre(numDia, horasViernes);
+                    break;
+                }
+              }
+            }
+          });
+      
+      
+          // ? Mostramos los datos de los areglos para generar la tabla
+          console.log(diaSemestre);
+      
+          setearTabla(alumnos);
+      
+          console.log(tbl);
+      
+      
+      
+      
+      
+      
+      
+      
+          // // ? Almacenamos los datos inhabiles:
+          // const inhabilDias = inHabilesInput
+          //   .split(",")
+          //   .map((date) => new Date(date.trim()));
+          // // ? indicamos cuantos dias habiles tenemos
+          // let diasHabiles = 0;
+          // // ? Le pasamos la variable de inicio para recorrer cada día hasta la fecha de final
+          // let actual = new Date(inicio);
+      
+          // // ? Contamos cada día desde la fecha de inicio hasta la fecha final
+          // // ? Esto para excluir los fines de semana y posterirormente los días sin clase
+          // while (actual <= final) {
+          //   //
+          //   const diaSemana = actual.getDay();
+      
+          //   // Verificamos si el día es habil o no:
+          //   const noEsHabil = inhabilDias.some(
+          //     (inhabilDias) => actual.toDateString() === inhabilDias.toDateString()
+          //   );
+      
+          //   // Solo contamos si no es fin de semana
+          //   if (diaSemana !== 0 && diaSemana !== 6) {
+          //     diasHabiles++;
+          //   }
+          //   // Avanzamos al sigueente día:
+          //   actual.setDate(actual.getDate() + 1);
+          // }
+      
+          // ? Mostramos el peridodo:
+          // document.getElementById(
+          //   "semestreResult"
+          // ).innerText = `La cantidad de dáis es: ${diasHabiles}`;
+        } else {
+          // document.getElementById("semestreResult").innerText = `Ingresa las fechas`;
+          alert("ingrese las fechas");
+        }
+      },
         descargarPDF() {
             try {
                 const { jsPDF } = window.jspdf;
